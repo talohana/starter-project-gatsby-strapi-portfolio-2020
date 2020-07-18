b83f8368abcfb7cfbdf17c83531cb6d88d394d5f
talohana <tal.ohana.x@gmail.com>
2020-07-18 11:00:05 +0300

    After Jobs Section

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,11 +1,66 @@
-import React from "react"
-import Title from "./Title"
+import { graphql, Link, useStaticQuery } from "gatsby"
+import React, { useState } from "react"
 import { FaAngleDoubleRight } from "react-icons/fa"
-import { graphql, useStaticQuery } from "gatsby"
-import { Link } from "gatsby"
+import Title from "./Title"
+
+const query = graphql`
+  {
+    jobs: allStrapiJobs(sort: { fields: strapiId, order: DESC }) {
+      nodes {
+        strapiId
+        company
+        position
+        date
+        description {
+          id
+          name
+        }
+      }
+    }
+  }
+`
 
 const Jobs = () => {
-  return <h2>jobs component</h2>
+  const [jobIndex, setJobIndex] = useState(0)
+
+  const {
+    jobs: { nodes },
+  } = useStaticQuery(query)
+
+  const { company, position, date, description } = nodes[jobIndex]
+
+  return (
+    <section className="section jobs">
+      <Title title="jobs" />
+      <div className="jobs-center">
+        <div className="btn-container">
+          {nodes.map(({ strapiId, company }, index) => (
+            <button
+              key={strapiId}
+              onClick={() => setJobIndex(index)}
+              className={`job-btn ${index === jobIndex && "active-btn"}`}
+            >
+              {company}
+            </button>
+          ))}
+        </div>
+        <article className="job-info">
+          <h3>{position}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{date}</p>
+          {description.map(({ id, name }) => (
+            <div key={id} className="job-desc">
+              <FaAngleDoubleRight className="job-icon" />
+              <p>{name}</p>
+            </div>
+          ))}
+        </article>
+      </div>
+      <Link to="/about" className="btn center-btn">
+        more info
+      </Link>
+    </section>
+  )
 }
 
 export default Jobs
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Hero from "../components/Hero"
+import Jobs from "../components/Jobs"
 import Layout from "../components/Layout"
 import Services from "../components/Services"
 
@@ -8,6 +9,7 @@ export default () => {
     <Layout>
       <Hero />
       <Services />
+      <Jobs />
     </Layout>
   )
 }
