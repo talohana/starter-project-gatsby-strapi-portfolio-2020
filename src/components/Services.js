60584c1dfb9225fb1d23d5d156cdb507e6625474
talohana <tal.ohana.x@gmail.com>
2020-07-18 10:45:01 +0300

    After Services

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,8 +1,23 @@
 import React from "react"
-import Title from "./Title"
 import services from "../constants/services"
+import Title from "./Title"
+
 const Services = () => {
-  return <h2>services component</h2>
+  return (
+    <section className="section bg-grey">
+      <Title title="services" />
+      <div className="section-center services-center">
+        {services.map(({ id, icon, title, text }) => (
+          <article key={id} className="service">
+            {icon}
+            <h4>{title}</h4>
+            <div className="underline"></div>
+            <p>{text}</p>
+          </article>
+        ))}
+      </div>
+    </section>
+  )
 }
 
 export default Services
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Hero from "../components/Hero"
 import Layout from "../components/Layout"
+import Services from "../components/Services"
 
 export default () => {
   return (
     <Layout>
       <Hero />
+      <Services />
     </Layout>
   )
 }
