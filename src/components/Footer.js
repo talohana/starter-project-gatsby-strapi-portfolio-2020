27dac8c02833e470f2e731bc3b28e9e90c759830
talohana <tal.ohana.x@gmail.com>
2020-07-22 19:31:53 +0300

    After Footer and Sidebar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,15 @@
 import React from "react"
 import SocialLinks from "../constants/socialLinks"
+
 const Footer = () => {
-  return <h2>footer component</h2>
+  return (
+    <footer className="footer">
+      <div>
+        <SocialLinks styleClass="footer-links" />
+        <h4>copyright &copy; {new Date().getFullYear()}</h4>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
+import Footer from "./Footer"
 // import "../css/main.css"
 import Navbar from "./Navbar"
+import Sidebar from "./Sidebar"
 
 const Layout = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleSidebar = () => setIsOpen(previousValue => !previousValue)
+
   return (
     <>
-      <Navbar />
+      <Navbar toggleSidebar={toggleSidebar} />
+      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
       {children}
+      <Footer />
     </>
   )
 }
