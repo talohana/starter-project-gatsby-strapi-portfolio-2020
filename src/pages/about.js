ca5b40962aeddff3e1ed472a6cedfe1dbbbf9c0e
talohana <tal.ohana.x@gmail.com>
2020-07-23 09:33:26 +0300

    After about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,50 @@
+import { graphql } from "gatsby"
+import Image from "gatsby-image"
 import React from "react"
 import Layout from "../components/Layout"
-import { graphql } from "gatsby"
 import Title from "../components/Title"
-import Image from "gatsby-image"
-// ...GatsbyImageSharpFluid
-const About = () => {
-  return <h2>about page</h2>
+
+const About = ({ data: { about } }) => {
+  const { title, info, image, stack } = about
+
+  return (
+    <Layout>
+      <section className="about-page">
+        <div className="section-center about-center">
+          <Image fluid={image.childImageSharp.fluid} className="about-img" />
+          <article className="about-text">
+            <Title title={title} />
+            <p>{info}</p>
+            <div className="about-stack">
+              {stack.map(({ id, title }) => (
+                <span key={id}>{title}</span>
+              ))}
+            </div>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  query {
+    about: strapiAbout {
+      title
+      info
+      image {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      stack {
+        id
+        title
+      }
+    }
+  }
+`
 
 export default About
